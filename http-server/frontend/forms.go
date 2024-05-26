efd9ce3a712b75ae3b2a69e2669eafeee1a7e38c
Egor <32245758+JustCup@users.noreply.github.com>
2024-05-26 15:50:17 +0300

    add: frontend base request

--- http-server/frontend/forms.go
+++ http-server/frontend/forms.go
@@ -5,6 +5,14 @@ import (
 	"github.com/iwrk-platform/formam/v3"
 )
 
+type BaseRequest struct {
+	Id      string `json:"id,omitempty"`
+	LastKey string `json:"lastKey"`
+	Limit   int64  `json:"limit"`
+	Offset  int64  `json:"offset"`
+	Action  string `json:"action"`
+}
+
 func ParseForm[T any](ctx *fiber.Ctx, item T) *fiber.Error {
 	if mpd, err := ctx.MultipartForm(); err != nil {
 		return fiber.NewError(fiber.StatusBadGateway, err.Error())
